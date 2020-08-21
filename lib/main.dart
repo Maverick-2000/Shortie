fae136283bcb00081bf6a5095c580125af789ef0
Maverick-2000 <stevebobygeorge@gmail.com>
2020-08-21 16:17:56 +0530

    Added circular progress indicator and url verification

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:share/share.dart';
+import 'package:string_validator/string_validator.dart';
 
 void main() {
   runApp(myApp());
@@ -30,27 +31,22 @@ class homePage extends StatefulWidget {
   _homePageState createState() => _homePageState();
 }
 
-
-
 class _homePageState extends State<homePage> {
-
   List userURL = List();
   List item = List();
 
- Future <List> getdata() async {
-    
+  Future<List> getdata() async {
     //JSON Parser
     var url = 'https://api.shrtco.de/v2/shorten?url=${userURL.last}';
     var respons = await http.get(url);
     var result = jsonDecode(respons.body);
-    item.add (result['result']['short_link']); //dictionary parse
-    
+    item.add(result['result']['short_link']); //dictionary parse
+
     print(item);
     return item;
-    
   }
 
-   createAlertDialog(BuildContext context) {
+  createAlertDialog(BuildContext context) {
     //method for alertdialog
     //promise to return string
     TextEditingController customController =
@@ -68,13 +64,18 @@ class _homePageState extends State<homePage> {
                 elevation: 5.0,
                 child: Text("OK"),
                 onPressed: () {
+                  bool validURL = isURL(customController.text); //check if valid url
+
+                  print(validURL);
                   if (customController.text != null &&
-                      customController.text != "") {
-                       userURL.add(customController.text);
-                    
+                      customController.text != "" &&
+                      validURL) {
+                    print("This is ${customController.text}");
+
+                    userURL.add(customController.text);
+                    setState(() {});
                   }
 
-                  setState(() {});
                   Navigator.of(context).pop();
                 },
               )
@@ -91,56 +92,51 @@ class _homePageState extends State<homePage> {
         title: Text("Shortie"),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: FutureBuilder(
-          future : getdata(),
-          builder: (BuildContext context, AsyncSnapshot snapshot){
-            if (snapshot.data==null){
-              return Center(
-                child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-
-                children: [
-                  Icon(
-                    Icons.sentiment_very_dissatisfied,
-                    color: Colors.grey,
-                    size:80,
-                  ),
-                  Text(
-                    "No short links to display",
-                    style: TextStyle(
-                      color: Colors.grey[700],
-                      fontSize: 15,
-                      //fontWeight: FontWeight.bold
-                    ),
-                    
-                    
-                    ),
-                ]
-              )
-              );
-            } else {
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, index){
-                return ListTile(
-                  leading: Icon(Icons.link),
-                  title: Text(snapshot.data[index]),
-                  subtitle: Text(userURL[index]),
-                  onTap: (){
-                    Share.share('Check out the short link I just shared with the application Shortie: ${snapshot.data[index]}', subject: 'Shortie short link');
-                    print (snapshot.data[index]);
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder(
+            future: getdata(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.data == null) {
+                return Center(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                      Icon(
+                        Icons.sentiment_dissatisfied,
+                        color: Colors.grey,
+                        size: 80,
+                      ),
+                      Text(
+                        "No short links to display",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 15,
+                          //fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ]));
+              } else {
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: Icon(Icons.link),
+                      title: Text(snapshot.data[index]),
+                      subtitle: Text(userURL[index]),
+                      onTap: () {
+                        Share.share(
+                            'Check out the short link I just shared with the application Shortie: ${snapshot.data[index]}',
+                            subject: 'Shortie short link');
+                        print(snapshot.data[index]);
+                      },
+                    );
                   },
-
                 );
-              },
-
-            );
-            }
-
-          },
-        )
-      ),
+              }
+            },
+          )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           createAlertDialog(context).then((onValue) {
